ba3dcd2c7b57c1974029efb9c9524221443d28c1
20170375 <62216628+20170375@users.noreply.github.com>
2020-12-02 01:26:59 +0900

    bug fix
    sample image load only once

--- DietManager/app/src/main/java/com/cookandroid/dietmanager/MainActivity.java
+++ DietManager/app/src/main/java/com/cookandroid/dietmanager/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -93,30 +95,39 @@ public class MainActivity extends AppCompatActivity {
     // AVD에 이미지 샘플 넣어주기
     private void sampleLoad(){
         Bitmap bmp = BitmapFactory.decodeResource(getResources(),R.drawable.food);
-
         OutputStream fos;
+
         try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                ContentResolver resolver = getContentResolver();
-                ContentValues contentValues = new ContentValues();
-                contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "sample.jpg");
-                contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg");
-                contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.
-                        DIRECTORY_PICTURES);
+            ContentResolver resolver = getContentResolver();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "sample.jpg");
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg");
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.
+                    DIRECTORY_PICTURES);
+
+            String[] what = new String[]{ MediaStore.Images.ImageColumns.DATE_TAKEN,
+                    MediaStore.Images.ImageColumns._ID,
+                    MediaStore.Images.ImageColumns.MIME_TYPE,
+                    MediaStore.Images.ImageColumns.DATA };
+            Uri queryUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+            // 샘플 이미지 존재하는지 확인
+            Cursor cursor = resolver.query(queryUri, what, null,null,
+                    MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC");
+            if(cursor.getCount() > 0){
+                Toast.makeText(this,"샘플 이미지가 이미 있습니다." +
+                        "\n/Pictures/sample.jpg",Toast.LENGTH_SHORT).show();
+            } else{
                 Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                         contentValues);
                 fos = resolver.openOutputStream(Objects.requireNonNull(imageUri));
-            } else {
-                String imagesDir = Environment.getExternalStoragePublicDirectory(Environment.
-                        DIRECTORY_PICTURES).toString();
-                File image = new File(imagesDir, "sample.jpg");
-                fos = new FileOutputStream(image);
+                bmp.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                fos.close();
+
+                Toast.makeText(this, "샘플 이미지를 불러왔습니다." +
+                        "\n/Pictures/sample.jpg", Toast.LENGTH_LONG).show();
             }
-            bmp.compress(Bitmap.CompressFormat.PNG, 100, fos);
-            fos.close();
-            Toast.makeText(this, "샘플 이미지를 불러왔습니다." +
-                    "\n/Pictures/sample.jpg", Toast.LENGTH_LONG).show();
-        } catch (Exception e){
+        } catch (Exception e) {
             Toast.makeText(this,"Fail to load sample.jpg",Toast.LENGTH_LONG).show();
         }
     }
